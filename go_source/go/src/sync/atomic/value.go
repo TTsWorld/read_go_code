95e50eadd1286b40625009446ed27e492ca3761e
tianfav <tianfav@163.com>
2022-08-16 14:38:31 +0800

    add gonum & add go atomic.value comment

--- go_source/go/src/sync/atomic/value.go
+++ go_source/go/src/sync/atomic/value.go
@@ -28,10 +28,12 @@ type ifaceWords struct {
 func (v *Value) Load() (val any) {
 	vp := (*ifaceWords)(unsafe.Pointer(v))
 	typ := LoadPointer(&vp.typ)
+	// 如果当前的typ是 nil 或者^uintptr(0)，那就证明第一次写入还没有开始，或者还没完成，那就直接返回 nil （不对外暴露中间状态）
 	if typ == nil || typ == unsafe.Pointer(&firstStoreInProgress) {
 		// First store not yet completed.
 		return nil
 	}
+	// 根据当前看到的typ和data构造出一个新的interface{}返回出去。
 	data := LoadPointer(&vp.data)
 	vlp := (*ifaceWords)(unsafe.Pointer(&val))
 	vlp.typ = typ
@@ -41,6 +43,7 @@ func (v *Value) Load() (val any) {
 
 var firstStoreInProgress byte
 
+// 源码解读可以参考文章: https://blog.betacat.io/post/golang-atomic-value-exploration/
 // Store sets the value of the Value to x.
 // All calls to Store for a given Value must use values of the same concrete type.
 // Store of an inconsistent type panics, as does Store(nil).
@@ -48,19 +51,29 @@ func (v *Value) Store(val any) {
 	if val == nil {
 		panic("sync/atomic: store of nil value into Value")
 	}
+	// 通过unsafe.Pointer将现有的和要写入的值分别转成ifaceWords类型，这样我们下一步就可以得到这两个interface{}的原始类型（typ）和真正的值（data）。
 	vp := (*ifaceWords)(unsafe.Pointer(v))
 	vlp := (*ifaceWords)(unsafe.Pointer(&val))
 	for {
+		// 通过LoadPointer这个原子操作拿到当前Value中存储的类型。下面根据这个类型的不同，分3种情况处理。
 		typ := LoadPointer(&vp.typ)
+		// 第一次写入
+		//  一个Value实例被初始化后，它的typ字段会被设置为指针的零值 nil，所以第9行先判断如果typ是 nil 那就证明这个Value还未被写入过数据。那之后就是一段初始写入的操作
 		if typ == nil {
 			// Attempt to start first store.
 			// Disable preemption so that other goroutines can use
 			// active spin wait to wait for completion.
+			// runtime_procPin 这是runtime中的一段函数，具体的功能我不是特别清楚，也没有找到相关的文档。
+			// 这里猜测一下，一方面它禁止了调度器对当前 goroutine 的抢占（preemption），使得它在执行当前逻辑的时候不被打断，以便可以尽快地完成工作，因为别人一直在等待它。
+			// 另一方面，在禁止抢占期间，GC 线程也无法被启用，这样可以防止 GC 线程看到一个莫名其妙的指向^uintptr(0)的类型（这是赋值过程中的中间状态）。
+			// 使用CAS操作，先尝试将typ设置为^uintptr(0)这个中间状态。如果失败，则证明已经有别的线程抢先完成了赋值操作，那它就解除抢占锁，然后重新回到 for 循环第一步。
 			runtime_procPin()
 			if !CompareAndSwapPointer(&vp.typ, nil, unsafe.Pointer(&firstStoreInProgress)) {
 				runtime_procUnpin()
 				continue
 			}
+			// 如果设置成功，那证明当前线程抢到了这个"乐观锁”，它可以安全的把v设为传入的新值了（19~23行）。
+			// 注意，这里是先写data字段，然后再写typ字段。因为我们是以typ字段的值作为写入完成与否的判断依据的。
 			// Complete first store.
 			StorePointer(&vp.data, vlp.data)
 			StorePointer(&vp.typ, vlp.typ)
@@ -71,9 +84,11 @@ func (v *Value) Store(val any) {
 			// First store in progress. Wait.
 			// Since we disable preemption around the first store,
 			// we can wait with active spinning.
+			// 第一次写入还未完成 - 如果看到 typ字段还是^uintptr(0)这个中间类型，证明刚刚的第一次写入还没有完成，所以它会继续循环，“忙等"到第一次写入完成。
 			continue
 		}
 		// First store completed. Check type and overwrite data.
+		// 第一次写入已完成 - 首先检查上一次写入的类型与这一次要写入的类型是否一致，如果不一致则抛出异常。反之，则直接把这一次要写入的值写入到data字段。
 		if typ != vlp.typ {
 			panic("sync/atomic: store of inconsistently typed value into Value")
 		}
